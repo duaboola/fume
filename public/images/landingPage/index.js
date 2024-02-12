2e471df97681121446d810bb3609d8642ae6c699
Shibiliya Ismail <50785323+duaboola@users.noreply.github.com>
2024-02-12 12:54:03 +0300

    Add files via upload

---
+++ public/images/landingPage/index.js
@@ -0,0 +1,57 @@
+import profileLogo from "./Ellipse 9.jpg";
+import Logo from "./logo.png";
+import buyImg from "./buy-img.png";
+import heartImg from "./heart-img.png";
+import searchImg from "./search-img.png";
+import userImg from "./user-img.png";
+import Article1 from "./article-1.png";
+import Article2 from "./article-2.png";
+import Article3 from "./article-3.png";
+import brandImg from "./Rectangle 16.png";
+import arrowImg from "./arrow.png";
+import img1Half from "./img-1-half.png";
+import img1Full from "./img-1-full.png";
+import img2Half from "./img-2-half.png";
+import img2Full from "./img-2-full.png";
+import img3Half from "./img-3-half.png";
+import img3Full from "./img-3-full.png";
+import img4Half from "./img-4-half.png";
+import img4Full from "./img-4-full.png";
+import aboutUsImg1 from "./about-us-img-1.png";
+import aboutUsImg2 from "./about-us-img-2.png";
+import aboutUsImg3 from "./about-us-img-3.png";
+import arrow2Img from "./arrow-2.png";
+import visaImg from "./visa-img.png";
+import masterImg from "./master-img.png";
+import payPalImg from "./paypal-img.png";
+import plusIcon from "./plus-icon.png";
+
+export const IMAGES = {
+  PROFILE_LOGO: profileLogo,
+  LOGO: Logo,
+  BUY_IMG: buyImg,
+  HEART_IMG: heartImg,
+  SEARCH_IMG: searchImg,
+  USER_IMG: userImg,
+  ARTICLE1: Article1,
+  ARTICLE2: Article2,
+  ARTICLE3: Article3,
+  BRAND_IMG: brandImg,
+  ARROW_IMG: arrowImg,
+  IMG_1_HALF: img1Half,
+  IMG_1_FULL: img1Full,
+  IMG_2_HALF: img2Half,
+  IMG_2_FULL: img2Full,
+  IMG_3_HALF: img3Half,
+  IMG_3_FULL: img3Full,
+  IMG_4_HALF: img4Half,
+  IMG_4_FULL: img4Full,
+  ABOUT_US_IMG_1: aboutUsImg1,
+  ABOUT_US_IMG_2: aboutUsImg2,
+  ABOUT_US_IMG_3: aboutUsImg3,
+  ARROW_2_IMG: arrow2Img,
+  VISA_IMG: visaImg,
+  MASTER_IMG: masterImg,
+  PAY_PAL_IMG: payPalImg,
+  PLUS_ICON: plusIcon,
+};
